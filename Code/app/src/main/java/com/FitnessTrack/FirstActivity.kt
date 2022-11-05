5780a2aebd56e45b955725e07354d20dd8cfbee3
yiyanglin0102 <yiyanglin0102@gmail.com>
2022-11-05 13:34:45 -0400

    Ready to connect to Firebase and configure Gradle files

--- Code/app/src/main/java/com/FitnessTrack/FirstActivity.kt
+++ Code/app/src/main/java/com/FitnessTrack/FirstActivity.kt
@@ -1,12 +1,16 @@
 package com.fitnesstrack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
 
@@ -14,5 +18,14 @@ class FirstActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        val typeface: Typeface =
+            Typeface.createFromAsset(assets, "Oswald-Medium.ttf")
+        tv_app_name.typeface = typeface
+
+        Handler().postDelayed({
+            startActivity(Intent(this@FirstActivity, IntroActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
--- Code/app/src/main/java/com/FitnessTrack/IntroActivity.kt
+++ Code/app/src/main/java/com/FitnessTrack/IntroActivity.kt
@@ -1,4 +1,4 @@
-package com.FitnessTrack
+package com.fitnesstrack
 
 import android.content.Intent
 import android.graphics.Typeface
@@ -9,32 +9,22 @@ import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
 
-    /**
-     * This function is auto created by Android when the Activity Class is created.
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
-        //This call the parent constructor
         super.onCreate(savedInstanceState)
-        // This is used to align the xml view to this class
         setContentView(R.layout.activity_intro)
-
-        // This is used to hide the status bar and make the splash screen as a full screen activity.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
         val typeface: Typeface =
-            Typeface.createFromAsset(assets, "carbon bl.ttf")
+            Typeface.createFromAsset(assets, "Oswald-Medium.ttf")
         tv_app_name_intro.typeface = typeface
 
-        // TODO (Step 7: Add a click event for Sign Up btn and launch the Sign Up Screen.)
-        // START
         btn_sign_up_intro.setOnClickListener {
-
-            // Launch the sign up screen.
             startActivity(Intent(this@IntroActivity, SignUpActivity::class.java))
         }
-        // END
+        btn_sign_in_intro.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, SignInActivity::class.java))
+        }
     }
 }
